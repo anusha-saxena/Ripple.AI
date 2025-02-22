c255334c54df026526ba35a91ab246a7fa392c3c
Raiya Minhas <raiya.minhas@gmail.com>
2025-02-22 00:17:10 -0500

    recommit of flask server updates

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from pymongo import MongoClient
 <<<<<<< Updated upstream
 import os
-#git test comment
+
 app = Flask(__name__)
 
 client = MongoClient(os.getenv("MONGODB_URI"))
@@ -11,31 +11,13 @@ problems_collection = db.problems
 
 @app.route("/", methods=["GET"])
 def home():
-    return "Flask server is running!"
+    return "Flask server running :)"
 
 @app.route("/submit_problem", methods=["POST"])
 def submit_problem():
     data = request.json
     problems_collection.insert_one(data)
     return jsonify({"client_message": data}), 200
-=======
-
-app = Flask(__name__)
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client["testdb"]  
-collection = db["testcollection"]  
-
-@app.route('/')
-def home():
-    return jsonify({"message": "Flask server is running!"})
-
-@app.route('/add', methods=['POST'])
-def add_document():
-    data = request.json
-    result = collection.insert_one(data)
-    return jsonify({"inserted_id": str(result.inserted_id)})
->>>>>>> Stashed changes
 
 @app.route('/get', methods=['GET'])
 def get_documents():
