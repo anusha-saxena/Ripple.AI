ea28352da5ec4cd1d1e937dc0fb50125dd9154bf
Anusha S <82239429+anusha-saxena@users.noreply.github.com>
2025-02-22 00:12:27 -0500

    test

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from pymongo import MongoClient
 <<<<<<< Updated upstream
 import os
-
+#git test comment
 app = Flask(__name__)
 
 client = MongoClient(os.getenv("MONGODB_URI"))
