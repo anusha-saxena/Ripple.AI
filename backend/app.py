cc3b8dcdfa190e0db3f4f75c985a824601769b10
Anusha S <82239429+anusha-saxena@users.noreply.github.com>
2025-02-22 15:35:24 -0500

    actually was able to connect submission form inputs to MongoDB through implementing proper authentications. created a register page .html to register users into user database

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ import bcrypt
 
 app = Flask(__name__, template_folder = "frontend")
 
-MONGODB_URI = "mongodb+srv://anushasaxena06:QkVhYP3MT0WAhz5B@cluster0.8ibtd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGODB_URI = "mongodb+srv://anusha06:Akshat12!@cluster0.8ibtd.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(MONGODB_URI)
 #added databases in mongoDB Atlas basically
 db = client["water_rights"]
@@ -16,34 +16,35 @@ problems_collection = db["problems"]
 def home():
     return "Flask server running :)"
 
+
+
+#user registration
+@app.route("/register", methods=["GET"])
+def register():
+    return render_template("register.html")
+@app.route("/register", methods=["POST"])
+def register_post():
+    data = request.form
+    username = data.get("username")
+    password = data.get("password")
+    if not username or not password:
+        return jsonify({"error":"no complete :("}), 400
+   
+    hashed = bcrypt.hashpw(password.encode('utf-8')), bcrypt.gensalt()
+   #inserting into MONGODB Users Database!!!!! :>>
+    data = {
+        "username": username,
+        "password": hashed
+    }
+    result = users_collection.insert_one(data)
+    return jsonify({"message": "registered successfully :)"}), 200
+
+
 #login page
 @app.route("/login", methods=["GET"])
 def login_page():
     return render_template("login.html")  #link to html login page
 
-#user registration
-@app.route("/register", methods=["POST"])
-def register():
-    data = request.json
-    username = data.get("username")
-    password = data.get("password")
-
-    if not username or not password:
-        return jsonify({"error": "Username and password are required"}), 400
-
-    if users_collection.find_one({"username": username}):
-        return jsonify({"error": "Username already exists"}), 409
-
-    #hash password
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-    #database insert
-    users_collection.insert_one({
-        "username": username,
-        "password": hashed_password.decode('utf-8')  #store as string
-    })
-
-    return jsonify({"message": "User registered successfully"}), 201
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -75,11 +76,12 @@ def submit_problem_post():
     if not title or not description:
         return jsonify({"error": "not fully completed :("}), 400
     #insertingggg to mongo!
-        result = problems_collection.insert_one({
+    data = {
         "title": title,
         "description": description,
-        "category": category
-    })
+         "category": category
+    }
+    result = problems_collection.insert_one(data)
     return jsonify({"message": "submitted successfully :)"}), 200
 
 if __name__ == '__main__':
